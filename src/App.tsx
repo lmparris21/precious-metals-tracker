4ac61b44bd79399fd34d2d53f172635841084b71
lmparris21 <lp_chops@yahoo.com>
2026-04-19 17:51:19 -0500

    Fix stale form state when navigating between add and edit routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useParams } from 'react-router-dom'
 import Layout from './components/Layout'
 import Dashboard from './components/dashboard/Dashboard'
 import Collection from './components/collection/Collection'
 import PieceForm from './components/pieces/PieceForm'
 import Settings from './components/settings/Settings'
+
+function PieceFormRoute() {
+  const { id } = useParams()
+  return <PieceForm key={id ?? 'new'} />
+}
 
 export default function App() {
   return (
@@ -12,8 +17,8 @@ export default function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Dashboard />} />
           <Route path="/collection" element={<Collection />} />
-          <Route path="/pieces/new" element={<PieceForm />} />
-          <Route path="/pieces/:id/edit" element={<PieceForm />} />
+          <Route path="/pieces/new" element={<PieceFormRoute />} />
+          <Route path="/pieces/:id/edit" element={<PieceFormRoute />} />
           <Route path="/settings" element={<Settings />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
